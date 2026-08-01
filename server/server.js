7f93dfab832168cdfd55c51d07d3acfa213ce32c
William Binks <williamjbinks@gmail.com>
2026-08-01 16:46:44 +0100

    minor changes to fix port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { SafeToSpend } = require('./safetospend.js');
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
